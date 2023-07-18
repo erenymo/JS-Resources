62070cb70a0273085a034c94e8e76b085ff4179a
Eren <erenbalta342115@gmail.com>
2023-07-18 14:51:00 +0300

    Updated

--- 10-Advanced-DOM-and-Events/starter/script.js
+++ 10-Advanced-DOM-and-Events/starter/script.js
@@ -33,7 +33,7 @@ document.addEventListener('keydown', function (e) {
 ///////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////
-
+/*
 //// Selecting elements
 
 console.log(document.documentElement);
@@ -49,7 +49,9 @@ const allButtons = document.getElementsByTagName('button'); // all the elements 
 console.log(allButtons);
 
 console.log(document.getElementsByClassName('btn'));
+*/
 
+/*
 //// Creating and inserting elements
 
 const message = document.createElement('div'); // we have created element but didn't insert to the page
@@ -89,7 +91,8 @@ console.log(getComputedStyle(message).height); // getComputedStyle returns strin
 message.style.height =
   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px'; // use parseFloat to convert String to Number
 
-document.documentElement.style.setProperty('--color-primary', 'orangered');
+// setProperty
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
 
 //////// Attributes
 // Attributes -> for ex:  src, alt, class, id are attributes of img element
@@ -99,6 +102,127 @@ console.log(logo.alt);
 console.log(logo.src);
 console.log(logo.className);
 
+logo.alt = 'beautiful minimalist logo';
+
 // Non-standard
 console.log(logo.designer); // this is not standard property that expected to be on img element,
 console.log(logo.getAttribute('designer'));
+logo.setAttribute('company', 'Bankist'); // creates a new attribute with the name of company
+
+console.log(logo.src); // absolute form
+console.log(logo.getAttribute('src')); // relative form
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href); // shows absolute url
+console.log(link.getAttribute('href')); // shows relative url
+
+///// Data Attributes
+// we use usually when we work with UI especially when we need to store data in the UI
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c'); // not includes
+
+// dont use!
+logo.className = 'jonas'; // this will delete all the classes and replace with 'jonas'
+*/
+
+///// Implementing Smooth Scrolling
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const navLink = document.querySelector('.nav__link');
+const section1 = document.querySelector('#section--1');
+const section2 = document.querySelector('#section--2');
+const section3 = document.querySelector('#section--3');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect(); // get the coordinates of the element that we want to scroll to
+  console.log(s1coords);
+  // getBoundingClientRect() is basically relative to visible viewport.
+  console.log(e.target.getBoundingClientRect()); // e.target returns the element that triggered the event (btnScrollTo)
+  console.log(
+    'Current scroll (X/Y) : ',
+    window.pageXOffset,
+    window.pageYOffset
+  ); // pageYOffSet returns the distance between current position of viewport and top of the page.
+
+  console.log(
+    'height/width of viewport : ',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // // Scrolling
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // ); // this doesn't work very well because it relative to viewport
+
+  // // Implementing smoothness in old way
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // });
+
+  // Much modern way
+  section1.scrollIntoView({
+    behavior: 'smooth',
+  });
+});
+
+/*
+///////// Types of Events and Event handlers
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+};
+
+// 'mouseenter' is like hover effect in css
+// modern way
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
+
+// // another way of attaching an event listener to an element (on{event} property)
+// // old way
+// h1.onmouseenter = function (e) {
+//   alert('onmouseenter: Great! You are reading the heading :D');
+// };
+*/
+/////////// EVENT PROPAGATION : BUBBLING AND CAPTURING
+
+// Creating Random Color rgb(255,255,255);
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+console.log(randomColor());
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // // Stop propagation
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener(
+  'click',
+  function (e) {
+    this.style.backgroundColor = randomColor();
+    console.log('NAV', e.target, e.currentTarget);
+  }
+  //true
+); // if the third parameters of addEventListener is true, event handler will no longer listen bubbling events, but instead listen capturing events.
