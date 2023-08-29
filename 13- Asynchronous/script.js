bfd8d5ff9637a557551af6ecd6a5a17937f96b2f
Eren <erenbalta342115@gmail.com>
2023-08-29 17:11:28 +0300

    Latest Updates

--- 13- Asynchronous/script.js
+++ 13- Asynchronous/script.js
@@ -3,6 +3,32 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderCountry = function (data, className = '') {
+  const html = `
+        <article class="country ${className}">
+        <img class="country__img" src="${data.flags.png}" />
+        <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)}M people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰
+            </span>${data.currencies[0].name}</p>
+            </div>
+            </article>
+            `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  // countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = 1;
+};
+
 ///////////////////////////////////////
 /*
 const getCountryData = function (country) {
@@ -44,27 +70,6 @@ getCountryData('usa');
 getCountryData('turkey');
 getCountryData('germany');
 */
-
-const renderCountry = function (data, className = '') {
-  const html = `
-        <article class="country ${className}">
-        <img class="country__img" src="${data.flags.png}" />
-        <div class="country__data">
-        <h3 class="country__name">${data.name}</h3>
-        <h4 class="country__region">${data.region}</h4>
-        <p class="country__row"><span>👫</span>${(
-          +data.population / 1000000
-        ).toFixed(1)} people</p>
-            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-            <p class="country__row"><span>💰
-            </span>${data.currencies[0].name}</p>
-            </div>
-            </article>
-            `;
-
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
 
 /*
 const getCountryAndNeighbour = function (country) {
@@ -151,32 +156,119 @@ setTimeout(() => {
 //     });
 // };
 
-const renderError = function (msg) {
-  countriesContainer.insertAdjacentText('beforeend', msg);
-  countriesContainer.style.opacity = 1;
+/*
+    const getJSON = function (url, errorMsg = 'Something went wrong') {
+      return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} ${response.status}`);
+    
+    return response.json();
+  });
 };
 
+// const getCountryData = function (country) {
+  //   fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+  //     .then(
+//       response => {
+//         console.log(response);
+
+//         if (!response.ok)
+//           throw new Error(`Country not found ${response.status}`);
+
+//         return response.json();
+//       } // fulfilled
+//     )
+//     .then(data => {
+//       renderCountry(data[0]);
+//       // const neighbour = data[0].borders?.[0];
+//       const neighbour = 'saddsasas';
+
+//       // Country 2
+//       return fetch(
+//         `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
+//       );
+//     })
+//     .then(response => {
+//       if (!response.ok) throw new Error(`Country not found ${response.status}`);
+//       return response.json();
+//     })
+//     .then(data => renderCountry(data, 'neighbour'))
+//     .catch(err => {
+//       // this catch method throws an error if there is any problem in any place at the whole promise chain
+//       console.error(`${err} 💥💥💥`);
+//       renderError(`Something went wrong 💥💥 ${err.message}. Try Again`);
+//     })
+//     .finally(() => {
+//       // finally method will execute no matter if it is fulfilled or rejected
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
 const getCountryData = function (country) {
-  fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
-    .then(
-      response => response.json() // fulfilled
-    )
+  getJSON(
+    `https://countries-api-836d.onrender.com/countries/name/${country}`,
+    'Country not found'
+  )
     .then(data => {
       renderCountry(data[0]);
       const neighbour = data[0].borders?.[0];
 
+      if (!neighbour) throw new Error('No neighbour found!');
+
       // Country 2
-      return fetch(
-        `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
+      return getJSON(
+        `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`,
+        'Country not found'
       );
     })
-    .then(response => response.json())
     .then(data => renderCountry(data, 'neighbour'))
     .catch(err => {
+      // this catch method throws an error if there is any problem in any place at the whole promise chain
       console.error(`${err} 💥💥💥`);
-    }); // this catch method throws an error if there is any problem in any place at the whole promise chain
+      renderError(`Something went wrong 💥💥 ${err.message}. Try Again`);
+    })
+    .finally(() => {
+      // finally method will execute no matter if it is fulfilled or rejected
+      countriesContainer.style.opacity = 1;
+    });
 };
 
 btn.addEventListener('click', function () {
-  getCountryData('turkey');
+  getCountryData('poland');
 });
+
+getCountryData('australia');
+*/
+
+/*
+/////////////////////////////////////////////////////
+// Coding Challenge #1 (Geocode API)
+
+const whereAmI = function (lat, lng) {
+  fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`)
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      if (data.city === 'Throttled! See geocode.xyz/pricing')
+        throw new Error(`Problem with geocoding`);
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+
+      return fetch(
+        `https://countries-api-836d.onrender.com/countries/name/${data.country}`
+      );
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found ${res.status}`);
+
+      return res.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => console.error(`${err.message}`))
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+whereAmI(-33.933, 18.474);
+*/
